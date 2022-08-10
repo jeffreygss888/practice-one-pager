818e1dba1d92f95f3ab7371fc988c56e94333175
jeffreygss888 <jeffreyasilo.gss@gmail.com>
2022-08-10 16:53:39 +0800

    chore: Added Animation and brick-wall

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        texture: `url('/src/assets/brick-wall.webp')`,
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 100 },
+        },
+        fadeOut: {
+          from: { opacity: 100 },
+          to: { opacity: 0 },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 300ms ease-out',
+        fadeOut: 'fadeOut 300ms ease-out',
+      },
+    },
   },
   plugins: [],
 };
